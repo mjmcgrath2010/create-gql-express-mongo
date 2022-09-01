990166987acec1dd66be261d9b6e343b5a5577f7
Mike McGrath <mike@mikemcgrathonline.us>
2022-09-01 17:57:54 -0400

    fix

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 const fs = require("fs-extra");
+const path = require("path");
 const { execSync } = require("child_process");
 
 const runCommand = (command) => {
@@ -14,16 +15,18 @@ const runCommand = (command) => {
 
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth=1 git@github.com:mjmcgrath2010/graphql-api-template.git ${repoName}}`;
-const binSrcDir = `./generators`;
-const binDestDir = `./${repoName}/bin/generators`;
-const copyGeneratorsCommand = `mkdir ./${repoName}/bin && ${fs.copySync(
-  binSrcDir,
-  binDestDir,
-  {
-    overwrite: true,
-  }
-)}`;
-const installCheckoutCommand = `cd ${repoName} && yarn install`;
+const binSrcDir = path.resolve(process.cwd(), "/generators");
+const binDestDir = path.resolve(process.cwd(), "`/${repoName}/bin/generators`");
+const copyGeneratorsCommand = `mkdir ${path.resolve(
+  process.cwd(),
+  `/${repoName}/bin/generators`
+)} && ${fs.copySync(binSrcDir, binDestDir, {
+  overwrite: true,
+})}`;
+const installCheckoutCommand = `cd ${path.resolve(
+  process.cwd(),
+  `/${repoName}`
+)} && yarn install`;
 
 /**
  * CREATE
